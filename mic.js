ec57af3932d64018f1892a97aa5ad319060e45ca
Peter Day <peterday.main@gmail.com>
2023-12-07 23:25:10 +0000

    progress

--- mic.js
+++ mic.js
@@ -1,14 +1,14 @@
 let mic = {
     active: false,
     boot(){
-        let css = 'ul.genericmilk-mic {position: absolute;padding: 0;background: white;box-shadow: 0 2px 4px 0 rgba(0,0,0,0.08);border-radius: 10px;margin: 0;list-style: none;opacity: 1;margin-top: 0px;transition: opacity .1s ease-in-out, margin-top .1s ease-in-out;outline: none;white-space: nowrap;';
-        css += 'ul.genericmilk-mic li {min-width: 200px;}';
-        css += 'ul.genericmilk-mic li a {cursor: pointer;display: block;padding: 10px;}';
-        css += 'ul.genericmilk-mic li a:hover {background-color: #307ed1;color: white;}';
-        css += 'ul.genericmilk-mic li:first-child a {border-top-left-radius: 10px;border-top-right-radius: 10px;}';
-        css += 'ul.genericmilk-mic li:last-child a {border-bottom-left-radius: 10px;border-bottom-right-radius: 10px;}';
-        css += 'ul.genericmilk-mic li.red a {color: white;}';
-        css += 'ul.genericmilk-mic li.red a:hover {background-color: red;color: white;}';
+        let css = 'ul.genericmilk-mic {position: absolute;padding: 0;background: white;box-shadow: 0 2px 4px 0 rgba(0,0,0,0.08);border-radius: 10px;margin: 0;list-style: none;opacity: 1;margin-top: 0px;transition: opacity .1s ease-in-out, margin-top .1s ease-in-out;outline: none;white-space: nowrap;} ';
+        css += 'ul.genericmilk-mic li {min-width: 200px;} ';
+        css += 'ul.genericmilk-mic li a {cursor: pointer;display: block;padding: 10px;font-family:Helvetica;} ';
+        css += 'ul.genericmilk-mic li a:hover {background-color: #307ed1;color: white;} ';
+        css += 'ul.genericmilk-mic li:first-child a {border-top-left-radius: 10px;border-top-right-radius: 10px;} ';
+        css += 'ul.genericmilk-mic li:last-child a {border-bottom-left-radius: 10px;border-bottom-right-radius: 10px;} ';
+        css += 'ul.genericmilk-mic li.red a {color: red;} ';
+        css += 'ul.genericmilk-mic li.red a:hover {background-color: red;color: white;} ';
         css += 'ul.genericmilk-mic:empty {opacity: 0;margin-top: 10px;}';
         
         var head = document.head || document.getElementsByTagName('head')[0],
@@ -25,6 +25,12 @@ let mic = {
         let pullDown = document.createElement('ul');
         pullDown.classList.add('genericmilk-mic');
         document.body.appendChild(pullDown);
+
+        // on pullDown blur remove all li's
+        pullDown.addEventListener('blur', function(e) {
+            pullDown.innerHTML = '';
+        });
+
     },
     drop(e,pkg){
 
@@ -37,9 +43,9 @@ let mic = {
 
         // set pull down's inner html to empty
         pullDown.innerHTML = '';
-
+        console.log(pkg);
         // add each item to the pull down
-        pkg.forEach((value) => {
+        pkg.forEach((value) => {            
             let className = value.class ? value.class : '';
 
             let item = document.createElement('li');
@@ -88,3 +94,4 @@ let mic = {
         pullDown.focus();
     }
 };
+mic.boot();
